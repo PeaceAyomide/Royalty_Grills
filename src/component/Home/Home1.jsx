7e992a2aac8abd3c77d57b93b05cdcd4fcf351c6
Peace Ayomide <helaralex2000@gmail.com>
2025-03-29 15:14:25 +0100

    new

--- src/component/Home/Home1.jsx
+++ src/component/Home/Home1.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import bgimg from '../../assets/bgimg.png'
 
 const Home1 = () => {
+  const styles = `
+  .menu-button:hover {
+    background-color: #8a2710;
+  }
+`
+
+
   return (
     <section 
     className="relative flex items-center justify-center h-screen bg-cover bg-center ml-0 [@media(min-width:1200px)]:ml-[17rem]"
    
     style={{ backgroundImage: `url(${bgimg})` }} // Replace with your actual image path
   >
+     <style>{styles}</style>
     {/* Dark overlay */}
     <div className="absolute inset-0 bg-black/60" aria-hidden="true"></div>
     
@@ -16,7 +24,7 @@ const Home1 = () => {
     <h1 className="text-5xl [@media(max-width:700px)]:text-[2.5rem] font-bold mb-4 ">Welcome To D'Royalty Kitchen</h1>
       
      <p className="text-xl [@media(max-width:700px)]:text-[1.1rem] mb-8">Experience premium exquisite meals with exceptional service</p>
-      <button className="bg-[#ad3114] hover:bg-[#8a2710] px-8 py-3 rounded-lg font-medium transition-colors">
+      <button className="menu-button  bg-[#ad3114] hover:bg-[#8a2710] px-8 py-3 rounded-lg font-medium transition-colors">
       Explore Menu
       </button>
     </div>
